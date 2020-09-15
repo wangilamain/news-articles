8abb352979856245a2a0549000b36ad7daefefc1
wangilamain <wangilayng@gmail.com>
2020-09-15 16:20:40 -0400

    new commits

--- app/main/views.py
+++ app/main/views.py
@@ -1,21 +1,22 @@
 from flask import render_template
 from . import main
 from ..requests import get_article, get_sources
-
+​
 # Views
 @main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    general = get_article('top-headlines','category=general')
-    business = get_article('top-headlines', 'category=business')
-    health = get_article('top-headlines', 'category=health')
-    entertainment = get_article('top-headlines', 'category=entertainment')
+    general = get_article('top-headlines')
+    #business = get_article('top-headlines')
+    #health = get_article('sources')
+    #entertainment = get_article('top-headlines')
     title = 'Home- News Updates'
-    return render_template('index.html', title = title,general=general, business=business,health=health,entertainment=entertainment)
-
-
+    return render_template('index.html', title = title,general=general)
+    # return render_template('index.html', title = title,general=general,health=health)
+    
+​
 @main.route('/sources')
 @main.route('/sources/<id>')
 def news(id=None):
@@ -24,7 +25,7 @@ def news(id=None):
     '''
     title = 'Sources'
     sources = get_sources('sources')
-    news_from_source = get_article('top-headlines',f'sources={id}')
+    news_from_source = get_article('top-headlines')
     if id:
         return render_template('sources.html', title=title,news=news_from_source,id=id )
     else:
