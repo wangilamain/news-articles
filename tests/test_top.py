ac31cb2e1c7ab12656ee7e11f97aa31a5364ee5a
wangilamain <wangilayng@gmail.com>
2020-09-15 15:21:41 -0400

    deployment to heroku

---
+++ tests/test_top.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Top
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Article class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Article('ABC News','Donald Trump','Arrogance','lorem ipsum','www.wdhd.com','www.whjd.com','2020-08-10')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article,Top))
