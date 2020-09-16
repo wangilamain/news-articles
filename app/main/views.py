de5a788f735aa6ae8ae9cf9b914c9569fff625a9
wangilamain <wangilayng@gmail.com>
2020-09-16 12:09:08 -0400

    heroku deployments

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_article, get_sources
+from ..requests import get_article, get_sources,topheadlines
 ​
 # Views
 @main.route('/')
@@ -30,10 +30,17 @@ def news(id=None):
     '''
     view news articles page that is detailed
     '''
-    title = 'Sources'
-    sources = get_sources('sources')
-    news_from_source = get_article('top-headlines')
-    if id:
-        return render_template('sources.html', title=title,news=news_from_source,id=id )
+    general_news = get_sources('general')
+    business_news = get_sources('business')
+    technology_news = get_sources('technology')
+    health_news = get_sources('health')
+    entertainment_news = get_sources('entertainment')
+    sports_news = get_sources('sports')
+    article = get_articles(id)
+    if article:
+        for i in article:
+            name = i.name
     else:
-        return render_template('sources.html', title=title,sources_list = sources)
+         name = ""
+        
+    return render_template('article.html',general =general_news, business =business_news,technology = technology_news, health = health_news,entertainment = entertainment_news,sports = sports_news,article = article,name = name)
